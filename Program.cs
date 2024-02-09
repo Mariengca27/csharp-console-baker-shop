cb2b9198fed24283ea0808909616d05353fd2473
Mariana Machado <marimeltywork@outlook.com>
2024-02-08 22:31:55 -0300

    Readme, first functions and options

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿string warningToCustomer = " Warning: NOW you are inside of the Cake Maryanne Shop. Please, be careful with the cakes. ";
+
+void showTheWarningToCustomer()
+{
+    Console.WriteLine(warningToCustomer);
+    Console.WriteLine(@"
+
+██████╗░░█████╗░██╗░░██╗███████╗██████╗░  ░██████╗██╗░░██╗░█████╗░██████╗░
+██╔══██╗██╔══██╗██║░██╔╝██╔════╝██╔══██╗  ██╔════╝██║░░██║██╔══██╗██╔══██╗
+██████╦╝███████║█████═╝░█████╗░░██████╔╝  ╚█████╗░███████║██║░░██║██████╔╝
+██╔══██╗██╔══██║██╔═██╗░██╔══╝░░██╔══██╗  ░╚═══██╗██╔══██║██║░░██║██╔═══╝░
+██████╦╝██║░░██║██║░╚██╗███████╗██║░░██║  ██████╔╝██║░░██║╚█████╔╝██║░░░░░
+╚═════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝  ╚═════╝░╚═╝░░╚═╝░╚════╝░╚═╝░░░░░
+");
+}
+
+
+void showCakesAndPiesOptions()
+{
+    Console.WriteLine("\nClick 1 - To See The Cakes");
+    Console.WriteLine("Click 2 - To See The Pies");
+    Console.WriteLine("Click 3 - To See The Cupcakes");
+    Console.WriteLine("Click 4 - To Exit");
+}
+
+
+showTheWarningToCustomer();
+showCakesAndPiesOptions();
---
+++ README.md
@@ -0,0 +1,17 @@
+## First of all:
+
+1º You need to review the documentation of C#, and I've included additional links below to assist you in gaining a solid understanding of the fundamentals of .NET and ASP.NET.
+
+- [C# Documentation](https://learn.microsoft.com/pt-br/dotnet/csharp/)
+- [.NET Documentation](https://learn.microsoft.com/pt-br/dotnet/)
+- [ASP.NET Documentation](https://learn.microsoft.com/pt-br/aspnet/)
+- [Entity Framework Documentation](https://learn.microsoft.com/pt-br/ef/)
+
+
+## To start the project:
+
+1º Clone the repository + Install Visual Studio 2022
+2º Open the project in Visual Studio
+3º Run the project
+5º The project is running and you can use it. 
+
